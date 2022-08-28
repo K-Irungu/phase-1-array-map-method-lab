991c2187231cd9e2e08cb7db1d5bb2083079e152
Kevin Irungu <kevin.irungu@student.moringaschool.com>
2022-08-28 13:40:23 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -11,6 +11,32 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+  
+
+function titleCased() {
+  let tutorial;
+  let newArray = [];
+
+  for(let i = 0; i < tutorials.length; i++) {
+     tutorial = tutorials[i].split(' ');
+    // Split the tutorials array into separate strings
+
+    for(let i = 0; i < tutorial.length; i++) {
+      tutorial[i] = (tutorial[i][0].toUpperCase() + tutorial[i].slice(1));
+    }
+    // Here I looped through each of the individuals strings in the 
+    // tutorial title array, making each index have its first index as .toUpperCase
+    // and the rest of the index as is.
+
+
+    tutorial = tutorial.join(' '); 
+    newArray.push(tutorial);
+    // I then joined each of the strings in the tutorial array and .pushed them 
+    // into a new array.
+  }
+  
+  return newArray;
+  // Here I returned the new array
 }
+titleCased();
+  
---
+++ practice.js
@@ -0,0 +1,41 @@
+// const skiSchool = ["aki", "guadalupe", "lei", "aalam"];
+// // const rollCall = [];
+
+// // for(const student of skiSchool) {
+// //     rollCall.push(student + " the skier");
+// // }
+
+// // console.log(rollCall);
+
+
+// // const wanaFunzi = skiSchool.map(function(student) {
+// //     return (student + " the skier");
+// // })
+
+// // console.log(wanaFunzi);
+
+// // function studentRollCall(student) {
+// //     return (student + " the skier");
+// // };
+
+// // const rollCall = skiSchool.map(studentRollCall);
+// // console.log(rollCall);
+
+// // const rollCall = skiSchool.map(function(student) {
+// //     return (student + " the skier");
+// // });
+// // console.log(rollCall);
+
+// // const rollCall = skiSchool.map((student) => (student + " the skier"));
+// // console.log(rollCall);
+
+
+// const robots = [
+//     { name: "Johnny 5", modes: 5, isActivated: false },
+//     { name: "C3PO", modes: 3, isActivated: false },
+//     { name: "Sonny", modes: 2.5, isActivated: false },
+//     { name: "Baymax", modes: 1.5, isActivated: false },
+//   ];
+
+// const activatedRobots = robots.map((robot) => Object.assign({}, robot, {modes: robot.modes * 2, isActivated: true}));
+// console.log(activatedRobots);
